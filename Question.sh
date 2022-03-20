f629e0037e59779b86e91fe72e2b403502131bdb
rajeshpal652 <rajesh.pal.652@gmail.com>
2022-03-20 18:07:57 +0530

    Modified: Employee is present or absent

--- Question.sh
+++ Question.sh
@@ -2,3 +2,10 @@ echo "Welcome to Employee"
 echo "Wage Computation"
 echo "Program on Master Branch"
 
+random=$(( RANDOM % 2 ))
+if (( $random ==1 ))
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
